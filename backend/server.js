f3b2c1aa0f58483a367396cbca2b75e0f1318366
PavniDua <pavnidua10@gmail.com>
2025-05-18 02:47:30 +0530

    final changes made

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,18 @@ import articlesRoutes from "./routes/article.js";
 import authRoutes from "./routes/auth.js";
 import discussionsRoutes from "./routes/discussion.js";
 import profilesRoutes from "./routes/profile.js";
-import communityRoutes from "./routes/community.js"
+import communityRoutes from "./routes/community.js";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5001;
-
-
 
 app.use(cors({
   origin: "http://localhost:5173", 
@@ -29,12 +33,22 @@ app.get('/api/config/cloudinary', (req, res) => {
     uploadPreset: process.env.CLOUDINARY_UPLOAD_PRESET,
   });
 });
-// Routes
+
+
 app.use("/articles", articlesRoutes);
 app.use("/auth", authRoutes);
 app.use("/discussions", discussionsRoutes);
 app.use("/profiles", profilesRoutes);
-app.use("/community",communityRoutes)
+app.use("/community", communityRoutes);
+
+
+const frontendPath = path.resolve(__dirname, "../frontend/build");
+app.use(express.static(frontendPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
