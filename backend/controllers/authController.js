6b3faca31b102cc8796633a2b417260ce165855d
PavniDua <pavnidua10@gmail.com>
2025-05-19 00:12:46 +0530

    final deployment

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -12,7 +12,7 @@ export const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'Strict',
+    sameSite: 'None',
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
